764ca5a2bfa8e3bdefc5728e3516f134518e10de
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-05-11 16:37:40 +0200

    To promises

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
+    .then((res) => {
       if (err) return console.log(err.message);
       console.log(res.body.message);
 
       fs.writeFile("dog-img.txt", res.body.message, (err) => {
         if (err) return console.log(err.message);
-        console.log("Random dog image saved top file!");
+        console.log("Random dog image saved to file!");
       });
     });
 });
