b38c708be86f87d4c3d4bf8204e2018492258fc7
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-05-11 18:25:54 +0200

    Promise

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file 👎");
+      resolve(data);
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`).then((data) => {
+  console.log(data);
+});
+
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then((res) => {
-      if (err) return console.log(err.message);
       console.log(res.body.message);
 
       fs.writeFile("dog-img.txt", res.body.message, (err) => {
         if (err) return console.log(err.message);
         console.log("Random dog image saved to file!");
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
